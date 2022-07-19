17a5c9171c68806ac3aa862b9ae74b5eca38b825
afes.okta <afes.okta@gmail.com>
2022-07-19 16:30:21 +0700

    update halaman welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,9 @@
     <section class="masthead d-flex align-items-center justify-content-center">
         <div class="container col-md-6 align-items-center justify-content-center text-center">
             <div class="animate__animated animate__bounceInDown animate__fast">
-                <h1 class="text-uppercase text-white font-weight-bold pb-3">Vurtify - Boilerplate Project</h1>
-                <p class="text-white-75 font-weight-light mb-5">
-                    Get the best out of Laravel 8 - without dealing with the complexity of Jetstream and Tailwind.
-                    Instead, we are using Bootstrap 5 and Vue 3
+                <h1 class="text-uppercase text-white font-weight-bold pb-3">{{ config('app.name', 'Laravel') }}</h1>
+                <p class="text-white-75 font-weight-light mb-5" style="text-align: left">
+                    Sistem Informasi pengelolaan surat-menyurat secara digital/elektronik yang berguna untuk mempercepat penyampaian informasi surat dan disposisinya kepada pihak yang dituju di lingkungan Pemerintah Desa Gelangkulon. Surat yang dikelola terdiri dari surat masuk, surat keluar, dan disposisi surat sampai dengan pemberkasannya secara elektronik.
                 </p>
             </div>
             <a class="btn btn-primary btn-xl text-white animate__animated animate__bounceInUp animate__fast"
